b8ba91620f709397144ec6f27728d660ec5bb374
Marko Laharnar <marko.laharnar@gmail.com>
2020-01-31 20:18:57 +0100

    Enemy can detect player

---
+++ SpaceRepair/Assets/Scripts/EnemyDetectionOfPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class EnemyDetectionOfPlayer : MonoBehaviour
+{
+    [SerializeField] Player player;
+    public bool detectingPlayer = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // terribly slow code
+        player = GameObject.FindObjectOfType<Player>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == player.gameObject)
+        {
+            detectingPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == player.gameObject)
+        {
+            detectingPlayer = false;
+        }
+    }
+}
