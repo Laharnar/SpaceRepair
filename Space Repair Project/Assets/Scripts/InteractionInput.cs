2f7035fe25ace6e2bbd082450d0f35fc67ca7149
Marko Laharnar <marko.laharnar@gmail.com>
2020-01-31 20:45:22 +0100

    scripts moved to other project

---
+++ Space Repair Project/Assets/Scripts/InteractionInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionInput:MonoBehaviour {
+    [SerializeField] KeyCode interactionKey;
+
+    public List<InteractibleItem> itemsInRange = new List<InteractibleItem>();
+
+    public void OnItemEnterRange(InteractibleItem item)
+    {
+        itemsInRange.Add(item);
+    }
+    public void OnItemExitRange(InteractibleItem item)
+    {
+        itemsInRange.Remove(item);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(interactionKey))
+        {
+            for (int i = 0; i < itemsInRange.Count; i++)
+            {
+                itemsInRange[0].behaviour.OnInteractionActivated();
+            }
+        }
+    }
+}
