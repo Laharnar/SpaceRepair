a9dde2dfe3ac5db851242675aae7c46b4aa5a3a3
brin-p <niirbed@gmail.com>
2020-02-01 09:22:37 +0100

    Movement dodan, art dodan.

---
+++ Space Repair Project/Assets/exposeAxis.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exposeAxis : MonoBehaviour
+{
+
+    public float inputX;
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        Debug.Log(inputX);
+    }
+}
