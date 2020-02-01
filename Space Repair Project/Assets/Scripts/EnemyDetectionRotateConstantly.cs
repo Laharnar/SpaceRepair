887c090bfb5630c2ed2a92f3ac4b799cc7517d39
Marko Laharnar <marko.laharnar@gmail.com>
2020-02-01 16:56:37 +0100

    Ping pong enemy sight, with angle

---
+++ Space Repair Project/Assets/Scripts/EnemyDetectionRotateConstantly.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class EnemyDetectionRotateConstantly : MonoBehaviour {
+
+    public Transform detection;
+    EnemyDetectionOfPlayer detectionSrc;
+
+    public Vector3 rotateConstantlyBy;
+    public bool pingPong= false;
+    public bool goingLeft = true;
+    public float offsetAngle = 0;
+    public float offsetMinus = 0;
+    public float offsetPlus = 0;
+
+    private void Update()
+    {
+        
+        detection.Rotate(
+            goingLeft ? rotateConstantlyBy : -rotateConstantlyBy);
+        Debug.Log(detection.eulerAngles.z);
+        if (detection.eulerAngles.z > 175+ offsetAngle+offsetPlus)
+        {
+            goingLeft = false;
+        }
+        else if(detection.eulerAngles.z < 5+ offsetAngle+offsetMinus)
+        {
+            goingLeft = true;
+        }
+    }
+}
