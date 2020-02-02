5b9c7cc488f2213cd7ed08c6549e1a902c901d57
brin-p <niirbed@gmail.com>
2020-02-02 13:54:27 +0100

    Button collision fixed, other things fixed, bridge added, reactor movement fixed...

---
+++ Space Repair Project/Assets/bridgeActivation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bridgeActivation : MonoBehaviour
+{
+    [SerializeField] buttonOnOff activatingButton;
+    Transform openBridge;
+
+
+    void Start()
+    {
+        openBridge = transform.GetChild(0);
+    }
+
+    void Update()
+    {
+        if(activatingButton.activated)
+        {
+            openBridge.gameObject.SetActive(false);
+            GetComponent<Collider2D>().enabled = false;
+        }
+    }
+}
