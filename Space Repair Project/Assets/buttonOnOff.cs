02e425da5e7c75a48c923c120d4d3e0da0805db3
brin-p <niirbed@gmail.com>
2020-02-02 10:22:04 +0100

    Button dodan.

---
+++ Space Repair Project/Assets/buttonOnOff.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonOnOff : MonoBehaviour
+{
+    Transform child;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col) 
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("dekat zgor button");
+            child = transform.GetChild(0);
+            child.gameObject.SetActive(false);
+            GetComponent<Collider2D>().enabled = false;
+        }
+    }
+}
